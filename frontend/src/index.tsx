dee5149d87c85ba5c6e68e0773ad65106b602035
Mark Tai <mark@marktai.com>
2023-08-24 22:27:57 -0500

    add favicon

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -16,6 +16,7 @@ root.render(
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossOrigin="anonymous"
     />
+    <link rel="icon" href="./favicon.png">
     <BrowserRouter>
       <Routes>
         <Route index element={<Driller/>} />
